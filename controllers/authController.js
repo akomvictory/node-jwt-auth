795e46e40f429e8ac2327534faca3019ae21fbd0
Victory Akom <hellonetguba@gmail.com>
2021-02-12 15:54:01 +0100

    Added more fields  to signup form

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,10 +53,10 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, username, name, phone, proffession } = req.body;
 
   try {
-    const user = await User.create({ email, password });
+    const user = await User.create({ email, password, username, name, phone, proffession });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user._id });
--- models/User.js
+++ models/User.js
@@ -4,17 +4,37 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: true,
-    lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
+      type: String,
+      required: [true, 'Please enter an email'],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail => { }, 'Please enter a valid email']
   },
   password: {
-    type: String,
-    required: [true, 'Please enter a password'],
-    minlength: [6, 'Minimum password length is 6 characters'],
-  }
+      type: String,
+      required: [true, 'Please enter a password'],
+      minlength:[6, 'Minimum password length is 6 characters'],
+  },
+  username: {
+      type: String,
+      required: [true, 'Please enter a username'],
+      minlength:[4, 'Minimum username length is 4 characters'],
+  },
+  name: {
+      type: String,
+      required: [true, 'Please enter your name'],
+      minlength:[3, 'Minimum length for name is 3 characters'],
+  },
+  proffession: {
+      type: String,
+      required: [true, 'Please enter your proffession'],
+      minlength:[4, 'Minimum length for proffession is 3 characters'],
+  },
+  phone: {
+      type: String,
+      required: [true, 'Please enter your phone number'],
+      minlength:[11, 'Minimum length for phone is 11 characters'],
+  },
 });
 
 
